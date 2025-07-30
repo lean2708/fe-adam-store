58cdca136c1e1368e9d688808cdd6577af1f7b41
ManhDevFull <thanhmanhdangfa@gmail.com>
2025-07-30 15:34:06 +0700

    Update order item

--- src/app/(private)/orders/page.tsx
+++ src/app/(private)/orders/page.tsx
@@ -11,7 +11,7 @@ interface TabItem {
   label: string;
 }
 const tabList: TabItem[] = [
-  { key: 'PENDING', label: 'Đơn hàng chờ xử lý' },
+  { key: 'PENDING', label: 'Đơn hàng chờ xác nhận' },
   { key: 'PROCESSING', label: 'Đơn hàng đang xử lý' },
   { key: 'SHIPPED', label: 'Đơn hàng đã gửi đi' },
   { key: 'DELIVERED', label: 'Đơn hàng đã giao' },
@@ -21,13 +21,88 @@ const tabList: TabItem[] = [
 export default function OrderPage() {
   const [isLoading, setIsLoading] = useState(false)
   const [activeStatus, setActiveStatus] = useState<TabStatus>('PENDING');
-  const [data, setData] = useState<any>({})
+  const [data, setData] = useState<any>({
+    orderItems: [
+      {
+        id: 1,
+        unitPrice: 200000,
+        quantity: 1,
+        image: {
+          id: 0,
+          imageUrl: "https://imgs.search.brave.com/1wFAag6ytkkNPhKwFcQhlennSJKCYwCLseov2L_p3og/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wb3Mu/bnZuY2RuLmNvbS83/Nzg3NzMtMTA1ODc3/L3BzLzIwMjIwODE1/XzJBTDZtTlRMRVNh/UGV3TUpxS2RYdVdt/ZC5qcGc"
+        },
+        productVariant: {
+          id: 1,
+          color: {
+            id: 0,
+            name: "Den"
+          },
+          size: {
+            id: 0,
+            name: "XL"
+          },
+          product: {
+            id: 0,
+            name: "Áo phông thương hiệu Việt Nam..."
+          }
+        }
+      },
+      {
+        id: 2,
+        unitPrice: 200000,
+        quantity: 2,
+        image: {
+          id: 0,
+          imageUrl: "https://imgs.search.brave.com/1wFAag6ytkkNPhKwFcQhlennSJKCYwCLseov2L_p3og/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wb3Mu/bnZuY2RuLmNvbS83/Nzg3NzMtMTA1ODc3/L3BzLzIwMjIwODE1/XzJBTDZtTlRMRVNh/UGV3TUpxS2RYdVdt/ZC5qcGc"
+        },
+        productVariant: {
+          id: 1,
+          color: {
+            id: 0,
+            name: "Den"
+          },
+          size: {
+            id: 0,
+            name: "L"
+          },
+          product: {
+            id: 0,
+            name: "Áo phông thương hiệu Việt Nam..."
+          }
+        }
+      },
+      {
+        id: 3,
+        unitPrice: 250000,
+        quantity: 3,
+        image: {
+          id: 0,
+          imageUrl: "https://imgs.search.brave.com/1wFAag6ytkkNPhKwFcQhlennSJKCYwCLseov2L_p3og/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wb3Mu/bnZuY2RuLmNvbS83/Nzg3NzMtMTA1ODc3/L3BzLzIwMjIwODE1/XzJBTDZtTlRMRVNh/UGV3TUpxS2RYdVdt/ZC5qcGc"
+        },
+        productVariant: {
+          id: 1,
+          color: {
+            id: 0,
+            name: "Den"
+          },
+          size: {
+            id: 0,
+            name: "XL"
+          },
+          product: {
+            id: 0,
+            name: "Áo phông thương hiệu Việt Nam..."
+          }
+        }
+      },
+    ]
+  })
   const btnByStatus: Record<TabStatus, React.ReactNode> = {
-    PENDING: <><button className='w-60 px-4 mr-4 py-2 rounded-md border border-black'>Thay đổi địa chỉ nhận hàng</button><button className='w-60 px-4 py-2 bg-black text-white rounded-md'>Hủy</button></>,
-    PROCESSING: <><button className='w-60 px-4 mr-4 py-2 rounded-md border border-black'>Thay đổi địa chỉ nhận hàng</button><button className='w-60 px-4 py-2 bg-black text-white rounded-md'>Hủy</button></>,
-    SHIPPED: <><p className="h-full flex items-center">{data.length != 0 && 'Dự kiến ngày nhận hàng: nhận hàng từ ngày 7/7/2025 đến 10/7/2025'}</p><button className='w-60 text-gray-400 px-4 mr-4 py-2 rounded-md border border-gray-400'>Xác nhận đã nhận đơn hàng</button></>,
-    DELIVERED: <><button className='w-60 text-gray-400 px-4 mr-4 py-2 rounded-md border border-gray-400'>Trả hàng/Hoàn tiền</button><button className='w-60 px-4 py-2 bg-black text-white rounded-md'>Đánh giá</button></>,
-    CANCELLED: <><button className='w-60 px-4 py-2 bg-black text-white rounded-md'>Mua lại</button></>
+    PENDING: <><button className='w-52 px-4 mr-4 py-2 rounded-md border border-black text-sm'>Thay đổi địa chỉ nhận hàng</button><button className='w-40 px-4 py-2 bg-black text-white rounded-md text-sm'>Hủy</button></>,
+    PROCESSING: <><button className='w-52 px-4 mr-4 py-2 rounded-md border border-black text-sm'>Thay đổi địa chỉ nhận hàng</button><button className='w-40 px-4 py-2 bg-black text-white rounded-md text-sm'>Hủy</button></>,
+    SHIPPED: <><button className='w-56 text-gray-400 px-4 py-2 rounded-md border border-gray-400 text-sm'>Xác nhận đã nhận được đơn</button></>,
+    DELIVERED: <><button className='w-28 px-4 py-2 bg-black text-white rounded-md text-sm'>Đánh giá</button></>,
+    CANCELLED: <><button className='w-28 px-4 py-2 bg-black text-white rounded-md text-sm'>Mua lại</button></>
   }
   useEffect(() => { getData() }, [activeStatus])
   const getData = async () => {
@@ -56,7 +131,10 @@ export default function OrderPage() {
                 activeStatus === tab.key
                 && 'border-b-3 border-black dark:border-white'
               )}
-              onClick={() => { setActiveStatus(tab.key); setData([]) }}
+              onClick={() => {
+                setActiveStatus(tab.key);
+                // setData([])
+              }}
             >
               {tab.label}
             </button>
@@ -64,25 +142,35 @@ export default function OrderPage() {
         </div>
         <div className="px-8 py-6">
           <div className='rounded-xl px-5 bg-gray-100'>
-            <h3 className='border-b-2 h-11 flex items-center justify-end border-gray-300'>
+            <h3 className='border-b-2 h-11 flex items-center justify-end border-gray-300 font-semibold uppercase'>
               {tabList.find(tab => tab.key === activeStatus)?.label}
             </h3>
             <div>
-              {isLoading && <div className="py-3 border-b-2 flex w-full justify-between h-24 items-center">
-                <Skeleton className='h-16 w-full' />
-              </div>}
-              {!isLoading && data?.orderItems?.length > 0 && data.orderItems.map((item: any, index: number) => <OrderItem key={item.id} item={item} />)}
+              {isLoading &&
+                <div className="py-3 border-b-2 flex w-full justify-between h-24 items-center">
+                  <Skeleton className='h-16 w-full' />
+                </div>}
+              {!isLoading && data?.orderItems?.length > 0 && data.orderItems.map((item: any, index: number) => {
+                const isLast = index === data.orderItems.length - 1;
+                return (
+                  <div key={item.id} className={clsx(
+                    'py-2',
+                    !isLast && 'border-b-2'
+                  )}>
+                    <OrderItem item={item} />
+                    <div className='py-2 text-end'>
+                      {btnByStatus[activeStatus]}
+                    </div>
+                  </div>
+                )
+              }
+              )}
 
               {!isLoading && (!data?.orderItems || data.orderItems.length === 0) && (
-                <p className="py-3 border-b-1 flex w-full h-16 items-center">Bạn chưa có đơn hàng nào cả</p>
+                <p className="py-3 border-b-1 flex w-full h-16 items-center justify-center">Bạn chưa có đơn hàng nào cả</p>
               )}
             </div>
-            <div className={clsx(
-              'py-4',
-              activeStatus === 'SHIPPED' ? 'flex justify-between' : 'text-end'
-            )}>
-              {btnByStatus[activeStatus]}
-            </div>
+
           </div>
         </div>
       </div>
--- src/components/ui/order-item.tsx
+++ src/components/ui/order-item.tsx
@@ -1,18 +1,21 @@
+import { formatCurrency } from "@/lib/utils"
+
 export default function OrderItem(props: any) {
   const { item } = props
   return (
-    <div className="py-3 border-b-2 flex w-full justify-between h-24 items-center">
+    <div className="py-3 flex w-full justify-between h-20 items-center">
       <div className="h-16 w-auto flex">
         <img className="h-16 w-16" src={item.image.imageUrl} alt={item.productVariant.product.name} />
         <div className="ml-3 flex flex-col justify-between">
-          <h4>{item.productVariant.product.name}</h4>
+          <h4 className="text-lg">{item.productVariant.product.name}</h4>
           <p className="text-gray-500">Size: {item.productVariant.color.name} - Màu sắc: {item.productVariant.size.name}</p>
         </div>
       </div>
       <div>
        <p className="text-gray-500 text-end">Số lượng: {item.quantity}</p>
-       <p className="text-end">{item.unitPrice} VNĐ</p>
-       <p>Tổng số tiền ({item} sản phẩm): <b>{item.quantity*item.unitPrice} VNĐ</b></p>
+       <p className="text-end">{formatCurrency(item.unitPrice)}</p>
+       
+       <p>Tổng số tiền ({item.quantity} sản phẩm): <b>{formatCurrency(item.unitPrice*item.quantity)}</b></p>
       </div>
     </div>
   )
