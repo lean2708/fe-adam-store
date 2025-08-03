440e9abd39da445832d434e9f974a936cf8c279d
Your Name <user@example.com>
2025-08-03 16:10:38 +0700

    update transform for address

--- src/lib/data/user.ts
+++ src/lib/data/user.ts
@@ -1,15 +1,47 @@
-import { UserControllerApi } from "@/api-client";
+import { AddressResponse, UserControllerApi } from "@/api-client";
 import { getAuthenticatedAxiosInstance } from "../auth/axios-config";
+import { TAddress } from "@/types";
 
 async function getAddressController() {
   const axiosInstance = await getAuthenticatedAxiosInstance();
   return new UserControllerApi(undefined, undefined, axiosInstance);
 
 }
+export async function transformAddressResponseToTAddress(apiAdress: AddressResponse): Promise<TAddress> {
+  return {
+    id: apiAdress.id,
+    isDefault: apiAdress.isDefault ?? false,
+    isVisible: apiAdress.isVisible,
+    status: apiAdress.status,
+    phone: apiAdress.phone,
+    streetDetail: apiAdress.streetDetail ?? '',
 
-export async function fetchAllAddressUserApi() {
+    ward: {
+      code: apiAdress.ward?.code,
+      name: apiAdress.ward?.name ?? '',
+    },
+
+    district: {
+      id: apiAdress.district?.id,
+      name: apiAdress.district?.name ?? '',
+    },
+
+    province: {
+      id: apiAdress.province?.id,
+      name: apiAdress.province?.name ?? '',
+    }
+  };
+}
+
+
+
+export async function fetchAllAddressUserApi(): Promise<TAddress[]> {
   const api = await getAddressController();
 
-  const response =await api.getAddressesByUser();
-  return response.data.result;
+  const response = await api.getAddressesByUser();
+  const items = response.data.result?.items ?? [];
+
+  return Promise.all(items.map(transformAddressResponseToTAddress));
+
 }
+
