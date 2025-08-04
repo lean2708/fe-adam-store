ec0826872e8994dd34f043a517114c81e3eb88b3
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-08-04 21:52:01 +0700

    style: change text size xl -> lg and remove flex for 2 action button

--- src/components/templates/(marketing)/Index/Product/(Details)/ProductActions.tsx
+++ src/components/templates/(marketing)/Index/Product/(Details)/ProductActions.tsx
@@ -11,18 +11,18 @@ export default function ProductActions({
   const t = useTranslations('Marketing.product_details');
 
   return (
-    <div className='flex gap-2 justify-center'>
+    <div className='flex gap-2 '>
       <Button
         variant={'outline'}
         onClick={onAddToCart}
-        className='px-10 py-3 flex-1 rounded-md font-semibold text-primary text-xl cursor-pointer'
+        className='px-10 py-3  rounded-md font-semibold text-primary text-lg cursor-pointer'
       >
         {t('product_infor.product_actions.add_to_cart')}
       </Button>
       <Button
         variant={'default'}
         onClick={onBuyNow}
-        className='px-10 py-3 flex-1 rounded-md font-semibold text-xl cursor-pointer'
+        className='px-10 py-3  rounded-md font-semibold text-lg cursor-pointer'
       >
         {t('product_infor.product_actions.buy_now')}
       </Button>
