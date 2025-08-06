6c1cd4e72a3c1d05a59a12b040e0632010c54de1
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-08-06 15:22:48 +0700

    fet: add updateCartItem action to cart store

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { TCartItem } from "@/types";
-import { create } from "zustand";
+import { TCartItem } from '@/types';
+import { create } from 'zustand';
 
 export type State = {
   cartItems: TCartItem[];
@@ -10,17 +10,40 @@ export type State = {
 
 export type Actions = {
   setCartItems: (cartItems: TCartItem[]) => void;
+  updateCartItem: (itemId: string, newData: Partial<TCartItem>) => void;
 };
 
 export const useCartStore = create<State & Actions>()((set) => ({
   cartItems: [],
-  totalPrice: "0",
+  totalPrice: '0',
   setCartItems: (cartItems) =>
     set(() => {
       let totalPrice = 0;
       cartItems.map((cartItem) => {
-        totalPrice += cartItem.quantity * Number(cartItem.Product?.colors?.[0]?.variants?.[0]?.price ?? 0);
+        totalPrice +=
+          cartItem.quantity *
+          Number(cartItem.Product?.colors?.[0]?.variants?.[0]?.price ?? 0);
       });
       return { cartItems, totalPrice: totalPrice.toFixed(2) };
+    }),
+  updateCartItem: (itemId, newData) =>
+    set((state) => {
+      const updatedCartItems = state.cartItems.map((item) =>
+        item.id === itemId ? { ...item, ...newData } : item
+      );
+
+      // Recalculate totalPrice
+      const totalPrice = updatedCartItems.reduce((sum, item) => {
+        return (
+          sum +
+          item.quantity *
+            Number(item.Product?.colors?.[0]?.variants?.[0]?.price ?? 0)
+        );
+      }, 0);
+
+      return {
+        cartItems: updatedCartItems,
+        totalPrice: totalPrice.toFixed(2),
+      };
     }),
 }));
