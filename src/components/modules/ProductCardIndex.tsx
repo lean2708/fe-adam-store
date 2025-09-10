ae37147d77bcf0eccac97151fa227c5eb1c637a5
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-09-10 23:59:42 +0700

    refactor: enhance size image

--- src/components/modules/ProductCardIndex.tsx
+++ src/components/modules/ProductCardIndex.tsx
@@ -56,16 +56,19 @@ export default function ProductCardIndex({
     <div className={cn('group relative cursor-pointer', className)}>
       {/* Product Image Container */}
       <div className='relative mb-4 aspect-[3/4] overflow-hidden rounded-xl bg-gray-50'>
-        <Link href={`/product/${product.id}`} className='block h-full w-full'>
+        <Link
+          href={`/product/${product.id}`}
+          className='block w-[286px] h-[429px]'
+        >
           <Image
             src={
               product.mainImage ||
               'https://images.pexels.com/photos/6069525/pexels-photo-6069525.jpeg?auto=compress&cs=tinysrgb&h=400&w=300placeholder-product.jpg'
             }
             alt={product.name || 'Product image'}
-            width={300}
-            height={400}
-            className='h-full w-full object-cover transition-transform duration-500 ease-out group-hover:scale-110'
+            fill
+            sizes='286px'
+            className='object-cover transition-transform duration-500 ease-out group-hover:scale-110'
           />
 
           {/* Badge */}
--- src/components/templates/(marketing)/Index/MainBanner/MainBannerSwiper.tsx
+++ src/components/templates/(marketing)/Index/MainBanner/MainBannerSwiper.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselPagination,
-} from "@/components/ui/carousel";
-import Image from "next/image";
+} from '@/components/ui/carousel';
+import Image from 'next/image';
 
 type HeroSlide = {
   src: string;
@@ -20,16 +20,17 @@ export default function MainBannerSwiper({
   heroSlides: HeroSlide[];
 }) {
   return (
-    <Carousel className="w-full">
+    <Carousel className='w-full'>
       <CarouselContent>
         {heroSlides.map((slide, idx) => (
           <CarouselItem key={idx}>
-            <div className="relative w-full h-[400px] md:h-[500px] lg:h-[600px]">
+            <div className='relative w-full h-[400px] md:h-[500px] lg:h-[600px]'>
               <Image
                 src={slide.src}
                 alt={slide.alt}
                 fill
-                className="object-cover"
+                sizes='(min-width: 1580px) 100vw, (min-width: 1040px) calc(15.58vw + 1317px), (min-width: 780px) 1236px, 995px'
+                className='w-full h-full object-cover'
                 priority={idx === 0}
               />
             </div>
